6bf6676e1c9fe2c3b5cffa55ec2fa3f8064d8949
Prince Caleb <kaylebprince@gmail.com>
2022-12-13 10:25:28 +0100

    removed the duplicate Name object

--- models/product.js
+++ models/product.js
@@ -4,10 +4,6 @@ const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  Name: {
-    type: String,
-    required: true,
-  },
   name: {
     type: String,
     required: true,
